053bb97148693f031c520aed1b692e321a9f09bd
ggxxhay <ggxxhay123@gmail.com>
2021-08-08 19:51:50 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import { Provider } from 'react-redux';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import './App.scss';
 import City from './components/City';
 import { store } from './store/store';
 
 function App() {
   return (
     <Provider store={store}>
-      <div id="app">
-        <City />
-      </div>
+      <Router>
+        <div id="app">
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/home" activeClassName="active">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" activeClassName="active">About</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/home">
+              <City />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   )
+}
+
+function About() {
+  return <h2>About</h2>;
 }
 
 export default App;
