21b0c8be4a7b30464cb383afdc59cd8ebc1b7904
ggxxhay <ggxxhay123@gmail.com>
2021-08-09 22:19:28 +0700

    Add header and tips

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import './App.scss';
@@ -9,28 +10,78 @@ function App() {
     <Provider store={store}>
       <Router>
         <div id="app">
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="/home" activeClassName="active">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/about" activeClassName="active">About</NavLink>
-              </li>
-            </ul>
-          </nav>
+          <Header />
 
           <Switch>
+            <Route exact path="/">
+              <City />
+            </Route>
             <Route path="/about">
               <About />
-            </Route>
-            <Route path="/home">
-              <City />
             </Route>
           </Switch>
         </div>
       </Router>
     </Provider>
+  )
+}
+
+function Header() {
+  const [hasError, setHasError] = useState(false);
+  const [tips, setTips] = useState(["Xin chào!"]);
+  const [tipIndex, setTipIndex] = useState(0);
+
+  const fetchTips = useCallback(async () => {
+    try {
+      const response = await fetch('http://localhost:3001/tips');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+      setTips(data);
+    } catch {
+      setHasError(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchTips();
+  }, [fetchTips])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!hasError) {
+        setTipIndex(revTipIndex => {
+          if (revTipIndex >= tips.length - 1) {
+            return 0;
+          } else {
+            return revTipIndex + 1;
+          }
+        });
+      }
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [hasError, tips])
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/" activeClassName="active">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" activeClassName="active">About</NavLink>
+        </li>
+
+        <div className="tips">
+          {hasError ? "Mọi chuyện vẫn ổn cả chứ?" : tips[tipIndex]}
+        </div>
+      </ul>
+    </nav>
   )
 }
 
